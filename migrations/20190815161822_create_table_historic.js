f026990751cf44ca1597a142cd8401872b77d7b3
Victor-132 <victormatheusmx132@gmail.com>
2019-08-24 11:54:13 -0300

    Ajuster na migration Historic

--- migrations/20190815161822_create_table_historic.js
+++ migrations/20190815161822_create_table_historic.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
     return knex.schema.createTable('historic', table => {
         table.increments('id').primary()
         table.integer('owner').notNull()
-        table.integer('moto').notNull()
+        table.string('moto').notNull()
         table.string('service').notNull()
-        table.dateTime('madeIn')
+        table.date('date')
     })
 };
 
