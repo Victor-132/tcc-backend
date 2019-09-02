51411c43a9d1ed7859a6aa3be2a2028526550686
Leandro <le.s.kuranaga@hotmail.com>
2019-09-02 00:04:24 -0300

    Puxando backend

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,17 @@
 module.exports = {
 
-  client: 'postgresql',
-  connection: {
-    database: 'mx7',
-    user:     'postgres',
-    password: 'matheus132'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
+    client: 'postgresql',
+    connection: {
+        database: 'mx7',
+        user: 'postgres',
+        password: 'suasenha@'
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }
 
 };
