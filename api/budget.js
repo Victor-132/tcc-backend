f2a5bad07a585de8b4b2f67ea619df47ed941487
Victor-132 <victormatheusmx132@gmail.com>
2019-12-03 00:17:13 -0300

    Alteração final

--- api/budget.js
+++ api/budget.js
@@ -4,7 +4,8 @@ module.exports = app => {
     const getBudget = (req, res) => {
         app.db('budget')
             .where({ id_users: req.user.id })
-            .orderBy('id')
+            .innerJoin('motorcycle', 'budget.id_motorcycle', 'motorcycle.id')
+            .orderBy('budget.id_motorcycle')
             .then(budget => res.json(budget))
             .catch(err => res.status(400).json(err))
     }
